6b6e567c26b3c40d7033e19afd79e694f9f6b873
LazuliKao <lazulikao@nbt.edu.cn>
2023-07-23 15:12:29 +0800

    feat: std_vector

--- src/native.cpp
+++ src/native.cpp
@@ -1,5 +1,7 @@
 #include <string>
-
+#include <vector>
+extern "C" {
+#pragma region std::string
 std::string* std_string_new() { return new std::string(); }
 std::string* std_string_new_str(const char* s) { return new std::string(s); }
 void std_string_delete(std::string* str) { delete str; }
@@ -10,3 +12,24 @@ void std_string_append_std_string(std::string* str, const std::string* s) {
 }
 void std_string_clear(std::string* str, const char* s) { str->clear(); }
 const char* std_string_data(std::string* str) { return str->data(); }
+#pragma endregion std::string
+
+#pragma region std::vector
+std::vector<char>* std_vector_new() { return new std::vector<char>(); }
+void std_vector_delete(std::vector<char>* vec) { delete vec; }
+void std_vector_push_back(std::vector<char>* vec, char* c, int length) {
+    while (length-- > 0) {
+        vec->push_back(*(c++));
+    }
+}
+char* std_vector_data(std::vector<char>* vec) { return vec->data(); }
+int std_vector_size(std::vector<char>* vec) { return vec->size(); }
+void* std_vector_at(std::vector<char>* vec, int index) {
+    return &vec->at(index);
+}
+void* std_vector_front(std::vector<char>* vec) { return &vec->front(); }
+void* std_vector_back(std::vector<char>* vec) { return &vec->back(); }
+void std_vector_pop_back(std::vector<char>* vec) { vec->pop_back(); }
+void std_vector_clear(std::vector<char>* vec) { vec->clear(); }
+#pragma endregion std::vector
+}
