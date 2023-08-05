5b079a91f71e5195caddaed75f063e5f9b02d83c
星梦 <starsdream00@icloud.com>
2023-08-05 11:48:58 +0800

    style: Fix warning

--- src/native.cpp
+++ src/native.cpp
@@ -27,30 +27,26 @@ void std_string_destructor(std::string* str) { str->~basic_string(); }
 
 std::string* std_string_new() { return new std::string(); }
 std::string* std_string_new_str(const char* s) { return new std::string(s); }
-void std_string_delete(std::string* str) {
-    if (str) delete str;
-}
-int std_string_length(std::string* str) { return str->size(); }
+void std_string_delete(std::string* str) { delete str; }
+unsigned long std_string_length(std::string* str) { return str->size(); }
 void std_string_append(std::string* str, const char* s) { str->append(s); }
 void std_string_append_std_string(std::string* str, const std::string* s) {
     str->append(*s);
 }
-void std_string_clear(std::string* str, const char* s) { str->clear(); }
+void std_string_clear(std::string* str) { str->clear(); }
 const char* std_string_data(std::string* str) { return str->data(); }
 #pragma endregion std::string
 
 #pragma region std::vector
 std::vector<char>* std_vector_new() { return new std::vector<char>(); }
-void std_vector_delete(std::vector<char>* vec) {
-    if (vec) delete vec;
-}
+void std_vector_delete(std::vector<char>* vec) { delete vec; }
 void std_vector_push_back(std::vector<char>* vec, char* c, int length) {
     while (length-- > 0) {
         vec->push_back(*(c++));
     }
 }
 char* std_vector_data(std::vector<char>* vec) { return vec->data(); }
-int std_vector_size(std::vector<char>* vec) { return vec->size(); }
+unsigned long std_vector_size(std::vector<char>* vec) { return vec->size(); }
 void* std_vector_at(std::vector<char>* vec, int index) {
     return &vec->at(index);
 }
@@ -69,8 +65,8 @@ void std_istream_new(char* buffer, int length, std::istream** stream,
     *stream = new std::istream(*sbuf);
 }
 void std_istream_delete(std::istream* stream, membuf* sbuf) {
-    if (stream) delete stream;
-    if (sbuf) delete sbuf;
+    delete stream;
+    delete sbuf;
 }
 bool std_istream_read(std::istream* stream, char* buffer, int length) {
     stream->read(buffer, length);
